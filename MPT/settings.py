a462c5ba4adeaa5c7826e812e1ba8995dc58a532
oqueffelec <octave.queffelec@soprasteria.com>
2020-04-06 00:17:04 +0200

    :rocket: Updated settings

--- MPT/settings.py
+++ MPT/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')klq7)957tc^#lmtd37#-4p^^&$0*3d+&46qnq5!8kpr5j!#pd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['monpetittennis.herokuapp.com']
 
 
 # Application definition
